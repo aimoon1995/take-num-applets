90546860d62e530baaee23d192863cc87c8fa0ec
aimoon23 <1297564418@qq.com>
2021-07-17 20:16:45 +0800

    tj

--- app.js
+++ app.js
@@ -2,34 +2,71 @@
 App({
   onLaunch() {
     let that = this;
-     // 获取用户信息
-     wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              that.globalData.userInfo = res.userInfo
-              console.log(res.userInfo)
-              let userInfo = res.userInfo
-              
-              wx.setStorageSync("userInfo", userInfo)
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+    
+    // 登录
+    let openid = that.globalData.token
+    console.log(that.globalData.token)
+    if(openid===null||openid===undefined){
+      wx.login({
+        success: res => {
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          wx.request({
+            method: 'POST',
+            url: that.globalData.serverUrl+'/moon/take/num/openid/get?code=' + res.code,
+            success: function (res) {
+              try {
+                that.globalData.userInfo = res.data.data
+                that.globalData.token = res.data.data.openid
+                wx.setStorageSync('token', res.data.data.openid)
+              } catch (e) {
               }
             }
           })
         }
-      }
-    })
+      })
+    }
+    
+    // if(openid!=null&&openid!=''){
+    //   // 获取用户信息
+    //   wx.getSetting({
+    //     success: res => {
+    //       if (res.authSetting['scope.userInfo']) {
+    //         // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //         wx.getUserInfo({
+    //           success: res => {
+    //             // 可以将 res 发送给后台解码出 unionId
+    //             that.globalData.userInfo = res.userInfo
+    //             console.log(res.userInfo)
+    //             let userInfo = res.userInfo
+                
+    //             wx.setStorageSync("userInfo", userInfo)
+
+    //             var params = 'nickName=' + userInfo.nickName + '&avatarUrl=' + userInfo.avatarUrl 
+    //             + '&gender=' + userInfo.gender + '&province=' + userInfo.province + '&city=' + userInfo.city 
+    //             + '&country=' + userInfo.country+"&"+that.globalData.tokenKey+"="+that.globalData.token;
+    //             wx.request({
+    //               url: that.globalData.serverUrl+'/api/user/auth.do?'+params,
+    //               method: 'POST',
+    //               success: function(res){
+    //                 console.log(res.data)
+    //               }
+    //             })
+    //             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //             // 所以此处加入 callback 以防止这种情况
+    //             if (this.userInfoReadyCallback) {
+    //               this.userInfoReadyCallback(res)
+    //             }
+    //           }
+    //         })
+    //       }
+    //     }
+    //   })
+    // }
   },
   globalData: {
     userInfo: null,
     tokenKey: 'API_TOKEN',
     token: null,
-    serverUrl: 'https://mini-mall.wx.api.dxq.pub',
+    serverUrl: 'http://192.168.100.152:8933'
   }
 })
--- pages/take/take.js
+++ pages/take/take.js
@@ -4,11 +4,14 @@ Page({
    * 页面的初始数据
    */
   data: {
+    serverUrl: 'http://192.168.100.152:8933',
     permCount:0,
-    haircutCount:0,
+    hairCutCount:0,
     permCountSec:0,
-    haircutCountSec:0,
-    name:null
+    hairCutCountSec:0,
+    name:null,
+    mobile:null,
+    token:null
   },
 
   /**
@@ -29,6 +32,30 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log(wx.getStorageSync('token')+"wx.getStorageSync('token')")
+    this.setData({
+      token:wx.getStorageSync('token')
+    })
+
+    this.getRuleNum()
+  },
+   
+  getRuleNum:function(){
+    let that = this;
+      // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      wx.request({
+        method: 'POST',
+        url: that.data.serverUrl+'/moon/take/num/type/count',
+        success: function (res) {
+          try {
+           that.setData({
+            permCount:res.data.data.permCount,
+            hairCutCount:res.data.data.hairCutCount
+           })
+          } catch (e) {
+          }
+        }
+      })
   },
 
   clickIcon:function (event) {
@@ -37,23 +64,118 @@ Page({
      name:event.detail.value
    });
   },
-
+  
+  nameClear:function (event) {
+    console.log(event.detail.value)
+   this.setData({
+     name:null
+   });
+  },
+  mobClear:function (event) {
+    console.log(event.detail.value)
+   this.setData({
+     mobile:null
+   });
+  },
+  mobClickIcon:function (event) {
+    console.log(event.detail.value)
+   this.setData({
+    mobile:event.detail.value
+   });
+  },
   permCountChange:function (event) {
    this.setData({
     permCountSec:event.detail
    });
   },
   
-  haircutCountChange:function (event) {
+  hairCutCountChange:function (event) {
    this.setData({
-    haircutCountSec:event.detail
+    hairCutCountSec:event.detail
    });
+  },
+
+  check:function name(name,mobile,permCountSec,haircutCountSec) {
+    // 检验
+    if (name == null || name =="") {
+      wx.showToast({
+        title: '称呼必填',
+        icon: 'error',
+        duration: 2000
+      })
+      return false;
+     } else if (mobile == null || mobile == "") {
+      wx.showToast({
+        title: '手机号必填',
+        icon: 'error',
+        duration: 2000
+      })
+      return false;
+    } else if (permCountSec == 0 && haircutCountSec == 0) {
+      wx.showToast({
+        title: '请预约人数',
+        icon: 'error',
+        duration: 2000
+      })
+      return false;
+    }else{
+      var myreg=/^[1][3,4,5,6,7,8,9][0-9]{9}$/;
+            if (!myreg.test(mobile)) {
+              wx.showToast({
+                title: '手机号格式有误',
+                icon: 'error',
+                duration: 2000
+              })
+              return false;
+            }  else {
+             return true;
+            }
+    }; 
   },
   takeNum:function() {
     var name = this.data.name;
+    var mobile = this.data.mobile;
     var permCountSec = this.data.permCountSec;
-    var haircutCountSec = this.data.haircutCountSec;
-    console.log("");
+    var hairCutCountSec = this.data.hairCutCountSec;
+    var openId = this.data.token
+    var res =this.check(name,mobile,permCountSec,hairCutCountSec);
+    if (res == true) {
+      wx.request({
+        url: this.data.serverUrl+'/moon/take/num/take',
+        data: {
+          permCount:permCountSec,
+          hairCutCount:hairCutCountSec,
+          name:name,
+          mobile:mobile,
+          openId:openId
+        },
+        method: "get",
+        success: function (res) {
+          console.log(res);
+        }
+      })
+    }
+  },
+
+
+  getPhoneNumber: function (e) {
+    var that = this;
+    console.log(e.detail.errMsg == "getPhoneNumber:ok");
+    if (e.detail.errMsg == "getPhoneNumber:ok") {
+      wx.request({
+        url: 'http://localhost/index/users/decodePhone',
+        data: {
+          encryptedData: e.detail.encryptedData,
+          iv: e.detail.iv,
+          sessionKey: that.data.session_key,
+          uid: "",
+        },
+        method: "post",
+        success: function (res) {
+          console.log(res);
+        }
+      })
+    }
   },
 
   /**
